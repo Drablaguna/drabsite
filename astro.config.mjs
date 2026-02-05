0c78c04b113f3b66164a5a9957944bfb6cfa3a41
Drablaguna <drablaguna@gmail.com>
2026-02-05 14:19:02 -0500

    ADD Coded broken site, consider rollback

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	
+	i18n: {
+		defaultLocale: 'es',
+		locales: ['es', 'en', 'pt'],
+		routing: {
+			prefixDefaultLocale: false
+		}
+	},
 	vite: {
 		plugins: [tailwindcss()],
 	},
